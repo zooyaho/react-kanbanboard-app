1acf89cc0d95be83cb200e35df28f3d81e542554
zooyaho <jiwoo1268@naver.com>
2022-06-14 20:39:24 +0900

    recoil의 seletor set함수 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { createGlobalStyle } from 'styled-components';
+import { hourSelector, minutesState } from './atom';
+
+// Reset CSS
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Roboto Mono', monospace;
+  background-color: white;
+  color:black;
+}
+a {
+  text-decoration:none;
+  color: inherit;
+}
+`;
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  // selector또한 state와 같이 배열로 받음.
+  const [hours, setHours] = useRecoilState(hourSelector);
+  console.log(hours);
+
+  const minuteChageHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value } } = event;
+    setMinutes(+value);
+  }
+
+  const hourChageHandler = (event:React.FormEvent<HTMLInputElement>)=>{
+    setHours(+event.currentTarget.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <input 
+        value={minutes} 
+        onChange={minuteChageHandler} 
+        type="number" 
+        placeholder='minutes' 
+      />
+      <input 
+        value={hours} 
+        onChange={hourChageHandler}
+        type="number" 
+        placeholder='hours' 
+      />
+    </>
   );
 }
 
