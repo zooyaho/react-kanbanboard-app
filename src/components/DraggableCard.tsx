79c01137a2c4b6229e3adbb0f35896d652a5d9b8
zooyaho <jiwoo1268@naver.com>
2022-06-15 14:48:28 +0900

    isDragging으로 dragging중일 때의 요소의 그림자 효과와 배경색 지정

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+interface ICard {
+  isDragging: boolean;
+}
+
+const Card = styled.div<ICard>`
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  // dragging중일 때의 배경색은 tomato
+  background-color: ${(props) => props.isDragging ? "tomato" : props.theme.cardColor};
+  box-shadow: ${props=> props.isDragging ? "0px 2px 5px rgba(0,0,0,0.3)": "none"}
 `;
 
 interface IDraggableCardProps {
@@ -19,8 +25,13 @@ const DraggableCard = ({toDo, index}:IDraggableCardProps) => {
   return (
     // 이 라이브러리에서 key와 draggableId의 값이 무조건 같아야 함.
     <Draggable draggableId={toDo} index={index}>
-      {(provied) => (
-        <Card ref={provied.innerRef} {...provied.dragHandleProps} {...provied.draggableProps} >
+      {(provied, snapshot) => (
+        <Card 
+          isDragging={snapshot.isDragging}
+          ref={provied.innerRef} 
+          {...provied.dragHandleProps} 
+          {...provied.draggableProps} 
+        >
           {toDo}
         </Card>
       )}
